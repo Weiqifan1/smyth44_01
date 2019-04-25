9cdb1aaedd920233a8be99a0c51c778c519801c2
Weiqifan1 <cmlykke@hotmail.com>
2019-04-25 09:34:13 +0200

    first commit kap. 44 smyth

---
+++ app/src/main/java/com/ebookfrenzy/lifecycledemo/DemoObserver.kt
@@ -0,0 +1,43 @@
+package com.ebookfrenzy.lifecycledemo
+
+import android.util.Log
+import android.arch.lifecycle.LifecycleObserver
+import android.arch.lifecycle.Lifecycle
+import android.arch.lifecycle.OnLifecycleEvent
+import android.arch.lifecycle.LifecycleOwner
+
+
+class DemoObserver: LifecycleObserver {
+    private val LOG_TAG = "DemoObserver"
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    fun onResume(){
+        Log.i(LOG_TAG, "onResume")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    fun onPause(){
+        Log.i(LOG_TAG, "onPause")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    fun onCreate(){
+        Log.i(LOG_TAG, "onCreate")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    fun onStart(){
+        Log.i(LOG_TAG, "onStart")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    fun onStop(){
+        Log.i(LOG_TAG, "onStop")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    fun onDestroy(){
+        Log.i(LOG_TAG, "onDestroy")
+    }
+
+}
